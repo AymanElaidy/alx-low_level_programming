d0972c4a4afd147d14cacbd7ace5b6c322d225b6
AymanElaidy <aymanelaidy20@gmail.com>
2023-03-30 16:55:25 -0700

    capitalize

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * cap_string - to replace the 1st lower letters in the words with upper ones
+ *
+ * @s: pointer to the array
+ *
+ * Return: string s
+ *
+ **/
+char *cap_string(char *s)
+{
+	int i;
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+	{
+		if (i == 0 && (s[i] >= 97 && s[i] <= 122))
+			s[i] = s[i] - 32;
+		if (s[i] == ' ' && (s[i + 1] >= 97 && s[i + 1] <= 122))
+			s[i + 1] = s[i + 1] - 32;
+	}
+	return (s);
+}
+
