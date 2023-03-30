d74202aaceefcb1cba8d72772d82a803987bf6b4
AymanElaidy <aymanelaidy20@gmail.com>
2023-03-30 15:55:11 -0700

    rev

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * reverse_array - to revers a sn array of integers
+ *
+ * @a: pointer to the array
+ * @n: array size
+ **/
+void reverse_array(int *a, int n);
+{
+	int i;
+
+	for (i = 0 ; i < n / 2 ; i++)
+	{
+		a[i] = a[n - i + 1];
+	}
+}
+
