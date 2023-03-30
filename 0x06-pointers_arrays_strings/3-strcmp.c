3d83c064e93b396403d31e3d95cc1ccaaf87ee42
AymanElaidy <aymanelaidy20@gmail.com>
2023-03-30 15:14:29 -0700

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strcmp - to compare strings
+ *
+ * @s1: 1st string
+ * @s2: 2nd one
+ *
+ * Return: (0) if s2 == s1 , (15) s1> s2 , (-15) if s2>s1
+ **/
+int _strcmp(char *s1, char *s2)
+{
+	int size = 0;
+
+	while (s2[size] != '\0')
+	{
+		if (s2[size] < s1[size])
+			return (-15);
+		else if (s2[size] < s1[size])
+			return (15);
+		else
+			return (0);
+	}
+}
+
