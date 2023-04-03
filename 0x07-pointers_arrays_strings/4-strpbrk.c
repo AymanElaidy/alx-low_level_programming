04f4ac46e40ef014bc60a1dfe309c95855417fa5
AymanElaidy <aymanelaidy20@gmail.com>
2023-04-03 16:45:17 -0700

    strspn impelementation

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strpbrk - to searche a string for any of a set of bytes
+ *
+ * @s: string to be passed
+ * @accept: to be searched for
+ *
+ * Return: pointer to the byte in s that matches the byte in substring
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int s_pos, acc_pos;
+
+	for (s_poss = 0 ; s[s_pos != '\0'] ; s_poss++)
+	{
+		for (acc_pos = 0 ; accept[a_pos] != '\0'; acc_pos++)
+		{
+			if (accept[acc_pos] == s[s_pos])
+				return (&s[s_poss]);
+		}
+	}
+	return (&s[s_poss]);
+}
