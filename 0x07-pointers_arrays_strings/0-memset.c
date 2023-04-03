ab0da48f41a63794fb31cd62fd415fd2d4a06949
AymanElaidy <aymanelaidy20@gmail.com>
2023-04-03 15:37:12 -0700

    memset() implementation

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _memset - fills memory with a constant byte
+ *
+ * @s: the string to be passed
+ * @b: the character to be added
+ * @n: size to be replaced
+ *
+ * Return: a pointer to resulted string
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int pos;
+
+	pos = 0;
+	while (n)
+	{
+		s[pos] = b;
+		pos++;
+		n--;
+	}
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Even more practcing on pointers, arries and strings
