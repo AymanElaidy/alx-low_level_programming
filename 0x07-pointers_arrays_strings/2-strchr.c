08bb505424e9cbf23a4eaa75a22e56e327c08de4
AymanElaidy <aymanelaidy20@gmail.com>
2023-04-03 16:10:39 -0700

    stcchr impelementation

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strchr -  locates a character in a string
+ *
+ * @s: string to be passed
+ * @c: character to search for
+ *
+ * Return: pointer to the first occurrence of the character
+ */
+char *_strchr(char *s, char c)
+{
+	int pos;
+
+	pos = 0;
+	while (s[0] != 0)
+	{
+		if (c == s[pos])
+			return (&s[0] + pos);
+		pos++;
+	}
+	return ('\0');
+}
