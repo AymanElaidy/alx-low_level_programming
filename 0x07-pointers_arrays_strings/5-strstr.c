099e5afc935f903e761fb727f7565f50636beb66
AymanElaidy <aymanelaidy20@gmail.com>
2023-04-03 17:25:07 -0700

    strstr() impelemintation

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strstr - to locate a substring
+ *
+ * @haystack: string to be passed
+ * @needle: string to search for
+ *
+ * Return:  pointer to the 1st occurence of the substring
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int pos1, pos2;
+
+	for (pos1 = 0 ; haystack[pos1] != '\0' ; pos1++)
+	{
+		for (pos2 = 0 ; needle[pos2] == haystack[pos1] ; pos2++)
+		{
+			if (needle[pos2] == '\0')
+				return (haystack);
+		}
+	}
+	return (NULL);
+}
