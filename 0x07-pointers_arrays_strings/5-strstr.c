5d91560677205a5a795f7b5cf97ab7c42fb78ce1
AymanElaidy <aymanelaidy20@gmail.com>
2023-04-03 17:53:36 -0700

    strstr() impelemintation

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -13,13 +13,14 @@ char *_strstr(char *haystack, char *needle)
 
 	for (pos1 = 0 ; haystack[pos1] != '\0' ; pos1++)
 	{
-		while ((needle[pos2] == haystack[pos1]) && (needle[pos2] != '\0'))
+		while (needle[pos2] != '\0')
 		{
-			pos1++;
+			if (haystack[pos1 + pos2] != needle[pos2])
+				break;
 			pos2++;
 		}
 		if (needle[pos2] == '\0')
-			return (&haystack[pos1]);
+			return (haystack + pos1);
 	}
 	return (NULL);
 }
