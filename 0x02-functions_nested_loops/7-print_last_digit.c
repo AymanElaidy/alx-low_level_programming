7ccd8feeed269dd02216008ac6b6fe2374dda616
root <aymanelaidy20@gmail.com>
2023-09-12 15:58:37 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_last_digit - to print the last digit
+ *
+ * @x: parameter
+ *
+ * return: the last digit
+ */
+int print_last_digit(int x)
+{
+	return (x % 10);
+}
