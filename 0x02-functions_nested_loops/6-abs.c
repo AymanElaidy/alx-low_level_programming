cf83c2059ee7f86d17d1dcb5f4b1532b6f98261f
root <aymanelaidy20@gmail.com>
2023-09-12 15:51:53 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - to get the absolute
+ *
+ * @x: to be checked
+ *
+ * Return: a for possitive or -a for negative
+ */
+int _abs(int x)
+{
+	if (x < 0)
+		return (-x);
+	else if (x >= 0)
+		return (x);
+}
